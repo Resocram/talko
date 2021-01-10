09e67924d06303f2584c85ae802a5182dc0eb228
cxsong1 <csca12.cs@gmail.com>
2021-01-09 23:58:24 -0800

    overall accuracy dashboard

--- talko-client/src/pages/App.js
+++ talko-client/src/pages/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-<<<<<<< HEAD
-import Recording from './Recording';
-=======
-import Speech from './Speech';
-import Tone from './Tone';
->>>>>>> 587634c6d619a10866b84823452afe14c05ec115
+import Dashboard from './Dashboard';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
-<<<<<<< HEAD
-      <Recording />
-=======
-      {/* <Speech /> */}
-      <Tone />
->>>>>>> 587634c6d619a10866b84823452afe14c05ec115
+      <Dashboard />
     </React.Fragment>
   );
 }
