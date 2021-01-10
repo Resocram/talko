ba735510577acec7f96d988a51d2eac3f7db8fba
JoshuaParkSJ <jpark9061@gmail.com>
2021-01-09 22:29:39 -0800

    fix: flask api interfaces stats

--- react-flask-test/flask-backend/main.py
+++ react-flask-test/flask-backend/main.py
@@ -1,67 +1,29 @@
 from flask import Flask
-from google.cloud import speech
-import io
-import wave, numpy as plt
-import syllables
-from fuzzywuzzy import fuzz
+import sys
+sys.path.insert(0, '../backend/stats.py')
+from stats import *
 
 app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def api():
-    x = getStats('a.wav', '../../audio-files')
-    print(x)
-    print('hello hello hello')
-    return x
-    # return {
-    #   stats: getStats('a.wav', '../../audio-files/a.wav'),
-    #   accuracy: getAccuracy(),
-    #   audioValues: getAudioValues(),
-    # }
-    
-def getStats(file, directory):
-    mysp=__import__("my-voice-analysis")
-    p=file # Audio File Name
-    c=directory # Path to the Audio File Directory
-    return mysp.mysptotal(p,c)   
+    stats = getStats("a", "../../audio-files")
+    print(stats)
 
-# def getAccuracy(userInput, transcribed):
-#     return fuzz.ratio(userInput,transcribed)
+    transcription = getTranscription("../../audio-files/a.wav")
+    print(transcription)
 
-# def getAudioValues(file):
-#     w = wave.open(file,"rb")
-#     p = w.getparams()
+    audioValues = getAudioValues("../../audio-files/a.wav")
+    print(audioValues)
 
-#     # Mono channel or dual channel
-#     channels = p[0]
+    userInput = """Hello! My name is Andrea sasir, I am studying marketing at the University of Texas at Dallas.
+            I am a member of American Marketing Association and Alpha Kappa PSI both of which are dedicated to 
+            shaping future Business Leaders. I hope to incorporate my business knowledge into consumer trend analysis
+            and strengthening relationships among consumers as well as other companies. I am savvy, social, and 
+            principles, and have exquisite interpersonal communciation skills. I know that I can be an asset in any
+            company and or situation and I hope that you will consider me for an internship or job
+            opportunities! Thank you so much!"""
 
-#     # Framerate of the audio file, usually in 44100Hz
-#     framerate = p[2]
-
-#     # Number of frames in the audio file
-#     frames = p[3]
-
-#     frameArray = w.readframes(frames)
-
-#     # How many datapoints we want to show 
-#     dataPoints = 10
-
-#     # Convert to an array of frames
-#     frameArray = numpy.fromstring(frameArray, numpy.int16)
-
-#     # Average non-mono channels into a mono channel
-#     frameArray = numpy.mean(frameArray.reshape(-1,channels),axis=1)
-
-#     # Remove extra frames when averaging
-#     additionalFrames = (len(frameArray) % (dataPoints))
-
-#     frameArray = frameArray[:-additionalFrames]
-#     frameArray = numpy.mean(frameArray.reshape(-1,int(len(frameArray)/dataPoints)),axis=1)
-
-#     # Normalize values between 0 and 1 
-#     normalize = (frameArray - numpy.min(frameArray)) / (numpy.max(frameArray) - numpy.min(frameArray))
-
-#     # plt.plot(normalize)
-#     # plt.show()
-
-#     return normalize
+    accuracy = getAccuracy(userInput,getTranscription("../../audio-files/a.wav"))
+    print(accuracy)
+    return 'heo'
