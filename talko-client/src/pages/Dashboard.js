56460f3f2695de586605da5f50a5d77aee88829c
Shade Wong <shadeying@alumni.ubc.ca>
2021-01-10 03:15:15 -0800

    Merge conflict :(

--- talko-client/src/pages/Dashboard.js
+++ talko-client/src/pages/Dashboard.js
@@ -4,12 +4,29 @@ import Button from '@material-ui/core/Button';
 import lightbulb from '../assets/lightbulb.svg';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
+import { Line } from 'react-chartjs-2';
+
+const state = {
+  labels: ['January', 'February', 'March',
+           'April', 'May'],
+  datasets: [
+    {
+      label: 'Rainfall',
+      fill: false,
+      lineTension: 0.5,
+      backgroundColor: 'rgba(75,192,192,1)',
+      borderColor: 'rgba(0,0,0,1)',
+      borderWidth: 2,
+      data: [65, 59, 80, 81, 56]
+    }
+  ]
+}
 
 function Dashboard() {
 	return (
         <div>
-            <Grid container spacing={1}>
-				<Grid item xs={3} style={{marginLeft:"50px"}}>
+            <Grid container spacing={1} style={{ height: '100%'}}>
+				{/* <Grid item xs={3} style={{marginLeft:"50px"}}>
                     <Box style={{height: "370px", width: "370px", borderRadius: "50%", backgroundColor: "#1B372D", marginTop:"-25px"}}/>
                     <Box style={{height: "285px", width: "285px", borderRadius: "50%", backgroundColor: "#3C7C64", marginTop: "-323px", marginLeft: "44px"}}/>
                     <Box style={{height: "230px", width: "230px", borderRadius: "50%", backgroundColor: "#52AA8A", marginTop: "-258px", marginLeft:"71px"}}/>
@@ -23,7 +40,21 @@ function Dashboard() {
                 <Grid item xs={2}>
 				</Grid>
                 <Grid item xs={2}>
-				</Grid>
+				</Grid> */}
+                <Line
+                    data={state}
+                    options={{
+                        title:{
+                        display:true,
+                        text:'Average Rainfall per month',
+                        fontSize:20
+                        },
+                        legend:{
+                        display:true,
+                        position:'right'
+                        }
+                    }}
+        />
 			</Grid>
         </div>
 		
