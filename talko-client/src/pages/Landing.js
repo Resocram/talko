fd1374b599fd62be991b317462efebad1bdada16
Claire S <cxsong1@DESKTOP-J3E3U5L.localdomain>
2021-01-09 19:48:46 -0800

    landing page basic format complete

--- talko-client/src/pages/Landing.js
+++ talko-client/src/pages/Landing.js
@@ -3,10 +3,10 @@ import Typography from '@material-ui/core/Typography';
 
 function Landing() {
 	return (
-		<div>
-			<Typography varaint="h1" style={{color: "#F2C407", fontSize: "400px"}}>Talko.</Typography>
-			<Typography varaint="h2"><strong>Description Bold</strong></Typography>
-			<Typography varaint="h3">Description Regular</Typography>
+		<div style={{width:"60%"}}> 
+			<Typography varaint="h1" style={{color: "#F2C407", fontSize: "180px", fontFamily: "Righteous", paddingLeft:"60px", marginTop: "30px"}}>Talko.</Typography>
+			<Typography varaint="h2" style={{color: "#F2C407", fontSize: "25px", fontFamily: "Montserrat", paddingLeft:"60px", paddingTop: "-10px"}}><strong>Receive INSTANT feedback on your public speaking skills.</strong></Typography>
+			<Typography varaint="h3" style={{color: "#F2C407", fontSize: "20px", fontFamily: "Montserrat", paddingLeft:"60px"}}>Talko will work with you to improve enunciation, pace, volume and much more. Input your speech now, and let's get talking!</Typography>
 		</div>
 	);
 }
