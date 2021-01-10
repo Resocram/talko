94c2594953f96a592920f8bca6bd371bea90a483
Claire S <cxsong1@DESKTOP-J3E3U5L.localdomain>
2021-01-09 19:18:38 -0800

    Talko heading

--- talko-client/src/pages/Landing.js
+++ talko-client/src/pages/Landing.js
@@ -4,7 +4,7 @@ import Typography from '@material-ui/core/Typography';
 function Landing() {
 	return (
 		<div>
-			<Typography varaint="h1">Talko.</Typography>
+			<Typography varaint="h1" style={{color: "#F2C407", fontSize: "400px"}}>Talko.</Typography>
 			<Typography varaint="h2"><strong>Description Bold</strong></Typography>
 			<Typography varaint="h3">Description Regular</Typography>
 		</div>
