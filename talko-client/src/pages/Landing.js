983036feba71cadac2d39f63e8fba63f0141ceca
Shade Wong <shadeying@alumni.ubc.ca>
2021-01-09 16:57:46 -0800

    test

--- talko-client/src/pages/Landing.js
+++ talko-client/src/pages/Landing.js
@@ -3,8 +3,8 @@ import Grid from '@material-ui/core/Grid';
 
 function Landing() {
 	return (
-		<Grid container direction="row" alignContent="center">
-			
+		<Grid container direction="row" alignContent="left">
+			<h1> Talko </h1>
 		</Grid>
 	);
 }
