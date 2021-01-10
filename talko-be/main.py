5f17f3f216478c2837eb90ede8adf33035d64fd8
JoshuaParkSJ <jpark9061@gmail.com>
2021-01-10 00:15:46 -0800

    fix: requirements.txt

--- talko-be/main.py
+++ talko-be/main.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def api():
-    stats = getStats("userInput","a","../../audio-files")
+    stats = getStats("userInput","a","../audio-files")
     print(stats)
 
     transcription = getTranscription("gs://talko/a.wav")
     print(transcription)
 
-    audioValues = getAudioValues("../../audio-files/a.wav")
+    audioValues = getAudioValues("../audio-files/a.wav")
     print(audioValues)
 
     userInput = """Hello! My name is Andrea sasir, I am studying marketing at the University of Texas at Dallas.
