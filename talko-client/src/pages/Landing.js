f2f954b79229bf1a548bcae21b0d39d09f578217
Shade Wong <shadeying@alumni.ubc.ca>
2021-01-09 17:55:49 -0800

    Rm router, fix navbar orient, remove grid in landing

--- talko-client/src/pages/Landing.js
+++ talko-client/src/pages/Landing.js
@@ -3,11 +3,11 @@ import Grid from '@material-ui/core/Grid';
 
 function Landing() {
 	return (
-		<Grid container direction="row" alignContent="left">
+		<div>
 			<h1>Talko.</h1>
 			<h2><strong>Description Bold</strong></h2>
 			<h3>Description Regular</h3>
-		</Grid>
+		</div>
 	);
 }
 
