24589fcb7f0f77b1e8c3a5df31fa78219d61a900
cxsong1 <csca12.cs@gmail.com>
2021-01-09 22:12:45 -0800

    speech page

--- talko-client/src/pages/App.js
+++ talko-client/src/pages/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import Landing from './Landing';
+import Speech from './Speech';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
-      <Landing />
+      <Speech />
     </React.Fragment>
   );
 }
--- talko-client/src/pages/Speech.js
+++ talko-client/src/pages/Speech.js
@@ -2,16 +2,26 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
-import Desktop from '../assets/desktop_nobkg.svg';
-import Grid from '@material-ui/core/Grid'; 
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
 
 function Speech() {
 	return (
 		<div> 
-            <Typography varaint="h2" style={{color: "#F2C407", fontSize: "20px", fontFamily: "Montserrat", paddingLeft:"60px", marginTop: "-20px"}}>What do you wanna say?</Typography>
-            <Typography varaint="h3" style={{color: "#F2C407", fontSize: "17px", fontFamily: "Montserrat", paddingLeft:"60px"}}>Talko will work with you to improve enunciation, pace, volume and much more. Input your speech now, and let's get talking!</Typography>
-            <Button style={{color: "white", fontSize: "26px", fontWeight:"bold", paddingLeft:"60px", marginTop: "-30px"}}>Ready, set, record!</Button>
-            <ArrowForwardIcon style={{fontSize: "40px", marginTop: "10px", color: "white"}} />
+            <Typography varaint="h2" style={{color: "#F2C407", fontSize: "68px", fontFamily: "Montserrat", marginLeft: "8%"}}>What do you wanna say?</Typography>
+            <Box style={{backgroundColor: "#1A2930", marginLeft: "8%", marginRight: "8%", border: "4px solid #F2C407", height: "230px", 
+                color: "#F2C407", fontSize: "35px", marginTop: "20px", paddingLeft: "40px", paddingTop: "20px",
+                borderRadius: "45px"}}>Enter your speech here...
+            </Box>
+            <form >
+                <TextField multiline placeholder="Enter your speech here..." style={{backgroundColor: "#1A2930", width: "85%", border: "4px solid #F2C407", height: "230px", 
+                color: "#F2C407", fontSize: "35px", marginTop: "20px", paddingLeft: "40px", paddingTop: "20px",
+                borderRadius: "45px"}}/>
+            </form>
+            <div style={{float: "right", marginRight: "8%", marginTop: "100px"}}>
+                <Button style={{color: "white", fontSize: "26px", fontWeight:"bold", paddingLeft:"60px", marginTop: "-30px"}}>Ready, set, record!</Button>
+                <ArrowForwardIcon style={{fontSize: "40px", marginTop: "10px", color: "white"}} />
+            </div>
 		</div>
 		
 	);
